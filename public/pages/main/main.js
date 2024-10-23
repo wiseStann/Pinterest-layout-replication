b130f07541bce0702395f8d9f70383e477555887
wiseStann <servicename18@gmail.com>
2024-10-23 16:00:55 +0300

    better positioning and actually ready to use

--- public/pages/main/main.js
+++ public/pages/main/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const DEFAULT_FULLPAGE_WIDTH = 1920;
-const DEFAULT_COLUMNS_N_FOR_FULLPAGE = 6;
+const DEFAULT_COLUMNS_N_FOR_FULLPAGE = 7;
 
 export class MainPage {
 	#parent;
@@ -31,10 +31,10 @@ export class MainPage {
 
 	buildLayout() {
 		const columnsN = this.adaptiveColumnsN;
-		const gutter = 40;
+		const widthGutter = 40, heightGutter = 15;
 		let heights = new Array(columnsN).fill(0);
 
-		const columnWidth = (this.parentContainerWidth - gutter * (columnsN + 1)) / columnsN;
+		const columnWidth = (this.parentContainerWidth - widthGutter * (columnsN + 1)) / columnsN;
 
 		for (const pin of this.#pins) {
 			const pinContainer = document.getElementById(`pin__content-container-${pin.PinID}`);
@@ -45,11 +45,14 @@ export class MainPage {
 			// Next column index to insert the pinContainer
 			const minIdx = heights.indexOf(Math.min(...heights));
 
-			pinContainer.style.top = `${heights[minIdx]}px`;
-			pinContainer.style.left = `${minIdx * columnWidth + gutter * (minIdx + 1)}px`;
+			pinContainer.style.top = `${heights[minIdx] + heightGutter}px`;
+			pinContainer.style.left = `${minIdx * columnWidth + widthGutter * (minIdx + 1)}px`;
 
-			heights[minIdx] += pinContainer.clientHeight;
+			heights[minIdx] += pinContainer.clientHeight + heightGutter;
 		}
+
+		const layoutContainer = document.querySelector('.feed__layout-container');
+		layoutContainer.style.height = `${Math.max(...heights)}px`;
 	}
 
 	get parentContainerWidth() {
